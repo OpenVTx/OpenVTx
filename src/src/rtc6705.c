7ff2ccad16acea9eb14ab31ff70271e7af950a63
Jye <JyeSmith@users.noreply.github.com>
2021-02-10 20:28:38 +1000

    fix Tramp power when changing frequencies

--- src/src/rtc6705.c
+++ src/src/rtc6705.c
@@ -93,4 +93,10 @@ void rtc6705WriteFrequency(uint32_t newFreq)
 
   /* Restore state */
   setPowerdB(myEEPROM.currPowerdB);
+  
+  /* Process uart data */
+  if (myEEPROM.vtxMode == TRAMP)
+    setPowermW(myEEPROM.currPowermW); // When in TRAMP mode power must be set by mW to stop rounding errors due to saved dBm being an int.
+  else
+    setPowerdB(myEEPROM.currPowerdB);
 }
