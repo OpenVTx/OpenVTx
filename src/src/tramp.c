c1929173414b692ade8c50666cf70e1a28809a7f
Jye <JyeSmith@users.noreply.github.com>
2021-05-02 08:19:08 +1000

    refract tramp setPower

--- src/src/tramp.c
+++ src/src/tramp.c
@@ -87,22 +87,16 @@ void trampProcessPPacket(void)
     mW <<= 8;
     mW += rxPacket[2];
     setPowermW(mW);
-
-    myEEPROM.currPowermW = mW;
-    updateEEPROM = 1;
 }
 
 void trampProcessIPacket(void)
 {
     pitMode = !rxPacket[2];
-
-     // Regardless of input mW, pitmode will force output to 0mW.
-    setPowerdB(myEEPROM.currPowerdB);
-
     myEEPROM.pitmodeInRange = pitMode;  // Pitmode set via CMS is not remembered with Tramp, but I have forced it here to be useful like SA pitmode.
     myEEPROM.pitmodeOutRange = 0;       // Set to 0 so only one of PIR or POR is set for smartaudio
 
-    updateEEPROM = 1;
+     // Regardless of input mW, pitmode will force output to 0mW.
+    setPowerdB(myEEPROM.currPowerdB);
 }
 
 void trampProcessSerial(void)
