25de7d080efe8d226a5983a6ac49238370550733
Jye <JyeSmith@users.noreply.github.com>
2021-06-06 08:20:14 +1000

    Fix L Band Freqs

--- src/src/targets/EWRF_E7082VM/EWRF_E7082VM.h
+++ src/src/targets/EWRF_E7082VM/EWRF_E7082VM.h
@@ -1,34 +1,3 @@
-/*
-
-# SMARTAUDIO
-# vtxtable
-vtxtable bands 5
-vtxtable channels 8
-vtxtable band 1 BOSCAM_A A FACTORY 5865 5845 5825 5805 5785 5765 5745 5725
-vtxtable band 2 BOSCAM_B B FACTORY 5733 5752 5771 5790 5809 5828 5847 5866
-vtxtable band 3 BOSCAM_E E FACTORY 5705 5685 5665 5645 5885 5905 5925 5945
-vtxtable band 4 FATSHARK F FACTORY 5740 5760 5780 5800 5820 5840 5860 5880
-vtxtable band 5 RACEBAND R FACTORY 5658 5695 5732 5769 5806 5843 5880 5917
-vtxtable powerlevels 5
-vtxtable powervalues 1 2 14 20 26
-vtxtable powerlabels 0 RCE 25 100 400
-
-# TRAMP
-# vtxtable 
-vtxtable bands 6
-vtxtable channels 8
-vtxtable band 1 BOSCAM_A A CUSTOM  5865 5845 5825 5805 5785 5765 5745 5725
-vtxtable band 2 BOSCAM_B B CUSTOM  5733 5752 5771 5790 5809 5828 5847 5866
-vtxtable band 3 BOSCAM_E E CUSTOM  5705 5685 5665 5645 5885 5905 5925 5945
-vtxtable band 4 FATSHARK F CUSTOM  5740 5760 5780 5800 5820 5840 5860 5880
-vtxtable band 5 RACEBAND R CUSTOM  5658 5695 5732 5769 5806 5843 5880 5917
-vtxtable band 6 IMD6     I CUSTOM  5732 5765 5828 5840 5866 5740    0    0
-vtxtable powerlevels 5
-vtxtable powervalues 1 2 25 100 400
-vtxtable powerlabels 0 RCE 25 100 400
-
-*/
-
 #ifndef __TARGET_DEF_H_
 #define __TARGET_DEF_H_
 
