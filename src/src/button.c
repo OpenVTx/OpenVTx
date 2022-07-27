5bae186251803a4ef08222241f17a60c18b4b6e1
Jye <JyeSmith@users.noreply.github.com>
2022-07-27 17:25:34 +1000

    add debounce and default option

--- src/src/button.c
+++ src/src/button.c
@@ -9,7 +9,9 @@
 #include "modeIndicator.h"
 #endif
 
+#define DEBOUNCE_TIME   50
 #define LONG_PRESS_TIME 1000
+#define DEFAULT_TIME    10000
 
 enum
 {
@@ -44,15 +46,23 @@ void checkButton(void)
     if (current_button_state == BUTTON_RELEASED && previous_button_state == BUTTON_PRESSED)
     {
         vtxModeLocked = 1;
-        
+
+        uint32_t elapsedTime = now - pressed_time_ms;
+
+        // debounce
+        if (elapsedTime < DEBOUNCE_TIME)
+        {
+            return;
+        } else
         // short press
-        if (now - pressed_time_ms < LONG_PRESS_TIME)
+        if (elapsedTime < LONG_PRESS_TIME)
         {
             myEEPROM.freqMode = 0;
             myEEPROM.channel = (myEEPROM.channel + 1) % FREQ_TABLE_SIZE;
             rtc6705WriteFrequency(channelFreqTable[myEEPROM.channel]);
         } else
         // long press
+        if (elapsedTime < DEFAULT_TIME)
         {
             uint8_t idx = 0;
             for (idx = 0; idx < SA_NUM_POWER_LEVELS; idx++)
@@ -61,7 +71,13 @@ void checkButton(void)
             }
             myEEPROM.currPowerdB = saPowerLevelsLut[(idx + 1) % SA_NUM_POWER_LEVELS];
             setPowerdB(myEEPROM.currPowerdB);
+        } else
+        // default EEPROM
+        {
+            defaultEEPROM();
+            rtc6705WriteFrequency(channelFreqTable[myEEPROM.channel]);
         }
+
         resetModeIndication();
     }
 }
